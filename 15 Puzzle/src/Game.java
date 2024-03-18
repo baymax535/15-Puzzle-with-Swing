7d2a976d889311200a5f8687b22f0ffd995cc345
Labib Alam <labibalam1100@gmail.com>
2024-03-18 11:48:13 -0500

    Trying to use a LinkedList Data Structure this time

--- 15 Puzzle/src/Game.java
+++ 15 Puzzle/src/Game.java
@@ -1,4 +1,26 @@
 
-public class Game {
-
+public class Game extends Main {
+	
+	Main main;
+    public Game(Main main) {
+        this.main = main;
+    }
+	public boolean gameRules() {
+		for(Node p = dummy; p!=null;) {
+			for(int i = 1; i<16 ;i++) {
+				if(p.next.data==i) {
+					p=p.next;
+				}
+				else return false;
+			}
+			return true;
+		}
+		return true;
+	}
+	public String checkGameRules() {
+		if(!gameRules())
+			return "Failed, Try Again!";
+		
+			return "You Won";
+	}
 }
--- 15 Puzzle/src/Main.java
+++ 15 Puzzle/src/Main.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-
+	protected Node dummy;
+	
+	static class Node{
+		int data;
+		Node next;
+		
+		Node(int i, Node n){
+			data = i;
+			next = n;
+		}
+	}
+	
+	Main(){
+		ArrayList<Integer> numbers = new ArrayList<>();
+        
+        // Adding numbers from 1 to 15 to the list
+        for (int i = 1; i <= 15; i++) {
+            numbers.add(i);
+        }
+        // Shuffling the list
+        Collections.shuffle(numbers);
+		dummy = new Node(0,null);
+		int i =1;
+		for(Node p = dummy; p!=null && i<16; p = p.next, i++) {
+			Node newNode = new Node(numbers.get(i), null);
+			dummy.next = newNode;
+		}
+	}
+	public void Start() {
+		new Main();
+	}
+	public void printList() {
+		for(Node p = dummy; p!=null;p=p.next) {
+			System.out.println(p.data);
+		}
+	}
+	
 }
